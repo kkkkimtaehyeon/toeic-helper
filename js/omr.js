296f989548f51cc73fa5354227ae70bfbcaab84d
kkkkimtaehyeon <48939950+kkkkimtaehyeon@users.noreply.github.com>
2024-01-22 02:59:50 +0900

    omr 체크 로직 수정

--- js/omr.js
+++ js/omr.js
@@ -6,126 +6,93 @@ const section5 = document.getElementById('section5');
 
 const lcBtn = document.getElementById('lcBtn');
 const rcBtn = document.getElementById('rcBtn');
-const omrSave = document.getElementById('omrSave');
+const saveBtn = document.getElementById('saveBtn');
 
-const choice = ['A', 'B', 'C', 'D'];
+const choices = ['A', 'B', 'C', 'D'];
 
 const circle = document.getElementsByClassName('circle');
 
 let userAnswerSheet = new Map();
 
-const checkTheAnswer = (event, aToDList) =>{
-
-    if(event.target.classList[1] === 'clicked'){
-    }
-    else{
-        for(let i = 0; i < aToDList.childNodes.length; i++){
-            
-            aToDList.childNodes[i].classList.remove('clicked');
-        }
-        event.target.classList.add('clicked');
-
-        console.log(aToDList.className);
-        console.log(aToDList.className.split(" ")[1]);
-        console.log(event.target.textContent);
-
-        let problemNumber = aToDList.className.split(" ")[1];
-        let userAnswer = event.target.textContent;
-
-        userAnswerSheet.set(problemNumber, userAnswer);
-        
-    }
+/**유저가 클릭한 답을 마킹*/
+const markingAnswer = (problemNumber, userAnswer) =>{
+    userAnswerSheet.set(problemNumber, userAnswer);
 }
-const attachIndexToProblem = (index, list) =>{//문제 번호를 붙여줌
+
+/**문제의 인덱스 번호*/
+const attachIndexToProblem = (index, list) =>{
     let span = document.createElement('span');
     span.innerHTML = index.toString() + '. ';
     list.appendChild(span);
 }
 
-const createAtoD = (section, start, end) => {
+const AtoD = (type, problemIndex) =>{
+    let divContainer = document.createElement('div');
+    let choicesLength;
+    if(type === 'LC' && (problemIndex >= 7 && problemIndex <= 31)){
+        choicesLength = 3;
+    }
+    else{
+        choicesLength = 4;
+    }
 
-    for(let j = start; j <= end; j++){
-        let index = j; 
-        let className = 'problem '+ index.toString();
+    for(let i = 0; i < choicesLength; i++){
+        let div = document.createElement('div');
 
-        let aToDList = document.createElement('li');
-        aToDList.className= className;
+        div.className = 'circle';
+        div.innerHTML = choices[i];
+        div.addEventListener('click', (event) =>{
+            for(let j = 0; j < divContainer.childNodes.length; j++){//모든 마킹 취소
+                divContainer.childNodes[j].classList.remove('clicked');
+            }
+            event.target.classList.add('clicked');//특정 보기에 마킹
 
-        attachIndexToProblem(index,aToDList);
+            console.log(divContainer.id);//문제 번호
+            console.log(event.target.textContent);//유저 답안
 
-        for (let i = 0; i < 4; i++) {
-            let div = document.createElement('div');
-    
-            div.className = 'circle';
-            div.innerHTML = choice[i];
-    
-            aToDList.appendChild(div);
-            
-        }
-        aToDList.addEventListener('click',(event)=>{
-            checkTheAnswer(event,aToDList);
+            markingAnswer(divContainer.id, event.target.textContent);
         });
 
-        section.appendChild(aToDList);
+        divContainer.appendChild(div);
+        divContainer.id = problemIndex;
     }
+
+    return divContainer;
 }
 
-const createAtoC = (section, start, end) => {
+/**섹션에 보기를 붙임*/
+const attachChoiceListOnSection = (type, section, start, end) => {
+    for(let i = start; i <= end; i++){
+        let index = i;
+        let className = 'problem ' + index.toString();
 
-    for(let j = start; j <= end; j++){
-        let index = j; 
-        let className = 'problem '+ index.toString();
+        let choiceList = document.createElement('div');
+        choiceList.className = className;
 
-        let aToCList = document.createElement('li');
-        aToCList.className= className;
+        attachIndexToProblem(index, choiceList);
 
-        attachIndexToProblem(index,aToCList);
+        choiceList.appendChild(AtoD(type, index));
 
-        for (let i = 0; i < 3; i++) {
-            let div = document.createElement('div');
-    
-            div.className = 'circle';
-            div.innerHTML = choice[i];
-
-            aToCList.appendChild(div);
-        }
-        aToCList.addEventListener('click', (event)=>{
-            checkTheAnswer(event,aToCList);
-        });
-
-        section.appendChild(aToCList);
+        section.appendChild(choiceList);
 
     }
-}
 
-const createOmrLc = () =>{
-    createAtoD(section1, 1, 6);
-    createAtoC(section1, 7, 20);
+};
 
-    createAtoC(section2, 21, 31);
-    createAtoD(section2, 32, 40);
+attachChoiceListOnSection('LC',section1,1,20);
+attachChoiceListOnSection('LC',section2,21,40);
+attachChoiceListOnSection('LC',section3,41,60);
+attachChoiceListOnSection('LC',section4,61,80);
+attachChoiceListOnSection('LC',section5,81,100);
 
-    createAtoD(section3, 41, 60);
-    createAtoD(section4, 61, 80);
-    createAtoD(section5, 81, 100);
-}
 
-const createOmrRc = () =>{
-    createAtoD(section1, 101, 120);
-    createAtoD(section2, 121, 140);
-    createAtoD(section3, 141, 160);
-    createAtoD(section4, 161, 180);
-    createAtoD(section5, 181, 200);
-}
-
-lcBtn.addEventListener('click', () => {
+/*lcBtn.addEventListener('click', () => {
     createOmrLc();
 });
 
 rcBtn.addEventListener('click', () => {
     createOmrRc();
-});
-
+});*/
 
 
 
